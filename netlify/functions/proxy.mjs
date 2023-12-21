2fc06800b2a45283d06930c8f28b01f44f0fbed0
mdkawsarali <facebookerjordan@gmail.com>
2023-12-22 01:54:36 +0600

    T5

--- netlify/functions/proxy.mjs
+++ netlify/functions/proxy.mjs
@@ -21,36 +21,48 @@
 //     }
 // };
 
-// proxy.js
-
 import axios from 'axios';
+import cheerio from 'cheerio';
 
 exports.handler = async function (event, context) {
     try {
-        // Fetch the entire HTML content including headers
-        const response = await axios.get('https://allthewheyup.com', { responseType: 'arraybuffer' });
+        // Fetch the content from the external website
+        const response = await axios.get('https://allthewheyup.com');
 
-        // Set the headers of the original response to the current response
-        const headers = response.headers;
-        Object.keys(headers).forEach((key) => {
-            context.headers[key] = headers[key];
-        });
+        // Clean up the HTML using cheerio
+        const cleanedHTML = cleanUpHTML(response.data);
 
-        // Return the HTML content as a buffer
+        // Return the cleaned HTML as the response
         return {
             statusCode: 200,
+            body: cleanedHTML,
             headers: {
-                'Access-Control-Allow-Origin': 'https://darling-elf-017308.netlify.app',
-                'Access-Control-Allow-Methods': 'GET, OPTIONS',
-                'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
+                'Content-Type': 'text/html',
             },
-            body: response.data.toString('base64'),
-            isBase64Encoded: true,
         };
     } catch (error) {
+        console.error('Error fetching content:', error);
+
+        // Return an error response
         return {
             statusCode: 500,
             body: 'Error fetching content',
+            headers: {
+                'Content-Type': 'text/plain',
+            },
         };
     }
 };
+
+function cleanUpHTML(html) {
+    // Load HTML into cheerio
+    const $ = cheerio.load(html);
+
+    // Clean up the HTML as needed
+    // For example, remove unnecessary elements, attributes, or whitespace
+    // Here, we're removing script tags for demonstration purposes
+    $('script').remove();
+
+    // Return the cleaned HTML
+    return $.html();
+}
