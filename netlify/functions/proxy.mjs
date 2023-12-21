a411d28096bda53862d16602a5f4fc5f07f63161
mdkawsarali <facebookerjordan@gmail.com>
2023-12-22 01:18:19 +0600

    Trying with my website

--- netlify/functions/proxy.mjs
+++ netlify/functions/proxy.mjs
@@ -1,35 +1,22 @@
-// src/functions/proxy.js
-
-import puppeteer from 'puppeteer';
+import axios from 'axios';
 
 exports.handler = async function (event, context) {
     try {
-        const browser = await puppeteer.launch();
-        const page = await browser.newPage();
-        await page.goto('https://www.amway.com/en_US/XS%E2%84%A2-Muscle-Multiplier---Berry-Blast-p-126753?utm_source=copy&utm_medium=sharebar&utm_campaign=us_en_3763670_9875642');
-
-        // Wait for some time to ensure dynamic content is loaded (adjust as needed)
-        await page.waitForTimeout(5000);
-
-        // Extract the HTML content after it has been fully loaded
-        const htmlContent = await page.content();
-
-        await browser.close();
-
+        const response = await axios.get('https://mdkawsarali.com');
+        // https://www.amway.com/en_US/XS%E2%84%A2-Muscle-Multiplier---Berry-Blast-p-126753?utm_source=copy&utm_medium=sharebar&utm_campaign=us_en_3763670_9875642
         return {
             statusCode: 200,
             headers: {
-                'Access-Control-Allow-Origin': 'https://darling-elf-017308.netlify.app',
+                'Access-Control-Allow-Origin': 'https://darling-elf-017308.netlify.app/',
                 'Access-Control-Allow-Methods': 'GET, OPTIONS',
                 'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
-                'Content-Type': 'text/html',
             },
-            body: htmlContent,
+            body: JSON.stringify(response.data),
         };
     } catch (error) {
         return {
             statusCode: 500,
-            body: JSON.stringify({ error: error }),
+            body: JSON.stringify({ error: 'Error fetching content' }),
         };
     }
 };
